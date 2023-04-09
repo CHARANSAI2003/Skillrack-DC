ae9fa85c66319f466af3e654ff2ff75adbbf5903
Charan Sai B <105934273+CHARANSAI2003@users.noreply.github.com>
2023-04-09 08:57:59 +0530

    Create Triangle Pattern - Hyphen and Asterisk.py

---
+++ Triangle Pattern - Hyphen and Asterisk.py
@@ -0,0 +1,40 @@
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section.
+
+Boundary Condition(s):
+1 <= N <= 100 
+
+Input Format:
+The first line contains the value of N. 
+
+Output Format:
+The first N lines containing the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1:
+Input:
+4
+
+Output:
+---**---
+--****-- 
+-******-
+********
+
+Example Input/Output 2:
+Input:
+7
+
+Output:
+------**------
+-----****-----
+----******----
+---********---
+--**********-- 
+-************- 
+**************
+
+-----------------------------------------------------------------------------------------------
+
+
+Program:
+
+  
