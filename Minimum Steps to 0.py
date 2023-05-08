1faa90a673883162df1a58a4745c6fea24efa513
Charan Sai B <105934273+CHARANSAI2003@users.noreply.github.com>
2023-05-08 20:05:17 +0530

    Update and rename Minimum Steps to 0 to Minimum Steps to 0.py

--- Minimum Steps to 0.py
+++ Minimum Steps to 0.py
@@ -33,3 +33,15 @@ Output:
 
 Program:
 
+n,k=list(map(int,input().split()))
+count = 0
+while n>0:
+    if n%k==0:
+        n//=k 
+        count+=1
+    else:
+        count+=n%k 
+        n-=n%k
+print(count)    
+
+
