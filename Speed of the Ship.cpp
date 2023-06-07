df39038db84d807b4cea2967ddf2bbdbab840b8d
Charan Sai B <105934273+CHARANSAI2003@users.noreply.github.com>
2023-06-07 16:24:12 +0530

    Update and rename Speed of the Ship to Speed of the Ship.cpp

--- Speed of the Ship.cpp
+++ Speed of the Ship.cpp
@@ -37,3 +37,18 @@ Output:
 
 Program:
 
+#include <iostream>
+ 
+using namespace std;
+
+int main()
+{
+    double x,y;
+    cin >> x >> y;
+    int c=0;
+    while(x>y){
+        y+=y/4;
+        c+=1;
+    }
+    cout << c; 
+}
